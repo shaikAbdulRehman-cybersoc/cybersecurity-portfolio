fcf456d125dcaf1f7c34c2d64e579d2e02f45887
shaikAbdulRehman-cybersoc <thundershaik040@gmail.com>
2026-03-09 20:32:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -29,3 +29,34 @@ setTimeout(type,150)
 }
 
 type()
+
+
+
+const terminalLines=[
+"> Initializing Cybersecurity Portfolio...",
+"> Loading Threat Intelligence Modules...",
+"> SOC Monitoring Active...",
+"> Access Granted ✔"
+]
+
+let i=0
+
+function terminal(){
+
+if(i<terminalLines.length){
+
+const p=document.createElement("p")
+
+p.textContent=terminalLines[i]
+
+document.querySelector(".terminal").appendChild(p)
+
+i++
+
+setTimeout(terminal,800)
+
+}
+
+}
+
+terminal()
