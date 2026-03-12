1c46fcf92647dcdc26cd680c3f0bb9b732b3a6b1
shaikAbdulRehman-cybersoc <thundershaik040@gmail.com>
2026-03-12 19:40:45 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,21 +7,16 @@ let letter=""
 
 function type(){
 
-if(count===text.length){
-count=0
-}
+if(count===text.length){count=0}
 
 current=text[count]
-
 letter=current.slice(0,++index)
 
 document.querySelector(".typing").textContent=letter
 
 if(letter.length===current.length){
-
 count++
 index=0
-
 }
 
 setTimeout(type,150)
@@ -29,6 +24,48 @@ setTimeout(type,150)
 }
 
 type()
+
+
+
+const canvas=document.getElementById("matrix")
+const ctx=canvas.getContext("2d")
+
+canvas.height=window.innerHeight
+canvas.width=window.innerWidth
+
+const letters="01ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const matrix=letters.split("")
+
+const fontSize=14
+const columns=canvas.width/fontSize
+const drops=[]
+
+for(let x=0;x<columns;x++)drops[x]=1
+
+function draw(){
+
+ctx.fillStyle="rgba(0,0,0,0.05)"
+ctx.fillRect(0,0,canvas.width,canvas.height)
+
+ctx.fillStyle="#00ff9c"
+ctx.font=fontSize+"px monospace"
+
+for(let i=0;i<drops.length;i++){
+
+const text=matrix[Math.floor(Math.random()*matrix.length)]
+
+ctx.fillText(text,i*fontSize,drops[i]*fontSize)
+
+if(drops[i]*fontSize>canvas.height && Math.random()>0.975)
+drops[i]=0
+
+drops[i]++
+
+}
+
+}
+
+setInterval(draw,33)
 
 
 
@@ -46,9 +83,7 @@ function terminal(){
 if(i<terminalLines.length){
 
 const p=document.createElement("p")
-
 p.textContent=terminalLines[i]
-
 document.querySelector(".terminal").appendChild(p)
 
 i++
